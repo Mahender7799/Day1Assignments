3948f37e40f0a6535698fb274325ad3c54042193
mahi <mahi@mahi-pc>
2021-04-15 23:29:12 +0545

    First Assignment

---
+++ SumOfTwoNumberCode/src/SumOfTwoNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumber {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First No :");
+		int firstNo=sc.nextInt();
+		System.out.println("Enter First No :");
+		int secoundNo=sc.nextInt();
+		int sum=firstNo+secoundNo;
+		System.out.println("Sum : "+firstNo+" + "+secoundNo+" = "+sum);
+	}
+
+}
