2349f8fdc8e81efc542d31fdc65d8460ba844b1e
mahi <mahi@mahi-pc>
2021-04-15 23:34:14 +0545

    First Assignment

---
+++ CommanLineArgument/src/CommanLineArgument.java
@@ -0,0 +1,11 @@
+
+public class CommanLineArgument {
+
+	public static void main(String[] args) {
+				int a = Integer.parseInt(args[0]);
+				String S=args[1];
+				System.out.println(a);
+				System.out.println(S);
+	}
+
+}
